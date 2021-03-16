f70c369fa0e635b67fdc5dc6685f2b1d9dab7c1b
Wyss <robin.wyss@dynatrace.com>
2021-03-16 08:44:07 +0100

    configure statsd exporter

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -1,12 +1,40 @@
 package com.example.demo;
 
+import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.statsd.StatsdConfig;
+import io.micrometer.statsd.StatsdFlavor;
+import io.micrometer.statsd.StatsdMeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
 	public static void main(String[] args) {
+//		StatsdConfig config = new StatsdConfig() {
+//			@Override
+//			public String get(String k) { return null; }
+//			@Override
+//			public StatsdFlavor flavor() { return StatsdFlavor.ETSY; }
+//
+//			@Override
+//			public int port() {
+//				return 8129;
+//			}
+//
+//			@Override
+//			public boolean enabled() {
+//				return true;
+//			}
+//		};
+//
+//		MeterRegistry registry = new StatsdMeterRegistry(config, Clock.SYSTEM);
+//
+//		Metrics.globalRegistry.add(registry);
+
 		SpringApplication.run(App.class, args);
 	}
+
 }
 
